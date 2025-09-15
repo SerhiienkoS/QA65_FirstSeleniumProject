53b29d21d8ce7a474be14efad3a755d8c5f99822
SerhiienkoS <sergey.sergienkow@gmail.com>
2025-09-15 10:27:53 +0200

    Add new LectionLast

--- src/test/java/com/phonebook/tests/CreateAccountTests.java
+++ src/test/java/com/phonebook/tests/CreateAccountTests.java
@@ -2,11 +2,20 @@ package com.phonebook.tests;
 
 import com.phonebook.models.User;
 import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 
 public class CreateAccountTests extends TestBase {
+
     SoftAssert softAssert = new SoftAssert();
+
+    @BeforeMethod
+    public void ensurePrecondition(){
+        if(!app.getUser().isLoginLinkPresent()){
+            app.getUser().clickOnSignOutButton();
+        }
+    }
 
     @Test(enabled = false)
     public void newUserRegistrationPositiveTest() {
